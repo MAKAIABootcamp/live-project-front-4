f1a6e24a71b30010694f193cb05bc84cbb69550d
LuisaLafaurie <m1m1respald0w0@gmail.com>
2023-07-05 21:38:49 -0500

    Guardando cambios

--- src/index.js
+++ src/index.js
@@ -2,19 +2,14 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import AppRouter from './router/AppRouter';
-import StudentsGeneral from './componentsGeneral/componentsSuperUser/StudentsOptions/StudentsGeneral';
-import Selection from './componentsGeneral/componentsSuperUser/Selection/Selection';
-
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    {/* <Provider>
+    <Provider>
       <AppRouter/>
-    </Provider> */}
-   {/* <Selection /> */}
-   <StudentsGeneral />
+    </Provider>
   </React.StrictMode>
 );
